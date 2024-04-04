f82437bfd8a27fe0c5045a4db4829613bc98b76e
Sunil <sunilsavale1993@gmail.com>
2024-04-04 13:29:33 +0530

    first java file

---
+++ CoreJavaDay1.java
@@ -0,0 +1,21 @@
+
+public class CoreJavaDay1 {
+
+	public static void main(String[] args) {
+		// To declare the variable 
+		// type VariableName = Value of Variable;
+		int MyNum = 5; //Integer Type
+		
+		String Mystring = "Hello World";
+		
+		double number = 1.99 ;
+		
+		char Mystr = 'S';
+		
+		System.out.println(MyNum);
+		System.out.println(Mystring);
+		System.out.println(number);
+		System.out.println(Mystr);
+		}
+
+}
